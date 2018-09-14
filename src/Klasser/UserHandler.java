7a336f7caaf7929d3beb4f256ed40f4c7ef67bfb
mariusjt <matjot@gmail.com>
2018-09-14 15:08:27 +0200

    Mye eksempelkode fra internett. Ikke bruk! Package Klasser for handling av vanlige MySQLmetoder

--- src/Klasser/UserHandler.java
+++ src/Klasser/UserHandler.java
@@ -1,4 +1,7 @@
 package Klasser;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 //Table for elever og lærere
 //Person ID (PK) = p_id
 //Rolle = p_role //om personen er teacher eller student(/TA?)
@@ -6,4 +9,17 @@ package Klasser;
 //Tlf = p_phone
 //Login og password?
 public class UserHandler {
+    public static void main(String[] args){
+        try(
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/slit", "root", "Chah1thA"
+        );
+        )
+    }
+    public void Create(int p_id, String user, String password, String email,
+                       String role, int phone){
+
+    }
+
+
 }
